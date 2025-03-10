6e4644a68762aac9c9a9779986e4df6bbf89c309
Enzo Fernandez <enzofc2003@gmail.com>
2025-03-10 15:19:36 -0300

    se agregan los a la paina admin

--- PoliProductivApp/app/admin.py
+++ PoliProductivApp/app/admin.py
@@ -1,5 +1,9 @@
 from django.contrib import admin
-from .models import Carrera, Materia
+from .models import Carrera, Materia, Profesor, Seccion, Horario, Evaluacion
 
 admin.site.register(Carrera)
 admin.site.register(Materia)
+admin.site.register(Profesor)
+admin.site.register(Seccion)
+admin.site.register(Horario)
+admin.site.register(Evaluacion)
